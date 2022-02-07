edf62d85be677aba213f253d5d40b72ff97721ed
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2022-02-07 11:31:13 +0100

    validation changed from button click to input focusout, validation function divided

--- paczkomat.js
+++ paczkomat.js
@@ -2,6 +2,7 @@ const modal = document.querySelector(".modal");
 const mainBtn = document.querySelector(".main-button");
 const form = document.querySelector(".form");
 const submitBtn = document.querySelector(".submit-button");
+submitBtn.disabled = true;
 
 mainBtn.addEventListener("click", function (){
     form.classList.contains("none") && form.classList.remove("none");
@@ -15,6 +16,7 @@ endBtn.addEventListener("click", function (){
     form.classList.add("none");
     modal.classList.add("none");
     mainBtn.classList.remove("none");
+    submitBtn.disabled = true;
 });
 
 const phone = document.getElementById("phone");
@@ -27,7 +29,68 @@ packageBtn.addEventListener("click", function (){
     modal.classList.add("none");
     phoneMsg.innerHTML = "";
     codeMsg.innerHTML = "";
+    submitBtn.disabled = true;
 });
+
+// I am creating an array to check if there are already two correct input values,
+// because I need to know when I can enable submit button
+let checkArray = [];
+
+function validatePhone() {
+    const phoneValue = document.getElementById("phone").value;
+    const phoneRegExp = /^\d{9}$/g;
+    let phoneResult = phoneRegExp.test(phoneValue);
+
+    if (phoneValue == ""){
+        phoneMsg.innerHTML = "Pole nie może byc puste";
+        phone.classList.add("error");
+    }
+    else if (phoneResult == false){
+        submitBtn.classList.remove("loading");
+        submitBtn.innerHTML = "Odbierz paczkę";
+        phoneMsg.innerHTML = "Niepoprawna wartość";
+        phone.classList.add("error");
+    }
+    else {
+        phoneMsg.innerHTML = "";
+        phone.classList.remove("error");
+        checkArray[0] = "phone";
+    }
+
+    if (checkArray.length == 2){
+        submitBtn.disabled = false;
+    }
+
+    return phoneResult;
+};
+
+function validateCode() {
+    const codeValue = document.getElementById("code").value;
+    const codeRegExp = /^\d{4}$/g;
+    let codeResult = codeRegExp.test(codeValue);
+
+    if (codeValue == ""){
+        codeMsg.innerHTML = "Pole nie może byc puste";
+        code.classList.add("error");
+    }
+    else if (codeResult == false){
+        submitBtn.classList.remove("loading");
+        submitBtn.innerHTML = "Odbierz paczkę";
+        codeMsg.innerHTML = "Niepoprawna wartość";
+        code.classList.add("error");
+    }
+    else {
+        codeMsg.innerHTML = "";
+        code.classList.remove("error");
+        checkArray[1] = "code";
+    }
+
+    if (checkArray.length == 2){
+        submitBtn.disabled = false;
+    }
+
+    return codeResult;
+};
 
 function validate() {
     const phoneValue = document.getElementById("phone").value;
@@ -39,28 +102,6 @@ function validate() {
     let phoneResult = phoneRegExp.test(phoneValue);
     let codeResult = codeRegExp.test(codeValue);
 
-    if (phoneResult == false){
-        submitBtn.classList.remove("loading");
-        submitBtn.innerHTML = "Odbierz paczkę";
-        phoneMsg.innerHTML = "Niepoprawna wartość";
-        phone.classList.add("error");
-    }
-    else {
-        phoneMsg.innerHTML = "";
-        phone.classList.remove("error");
-    }
-
-    if (codeResult == false){
-        submitBtn.classList.remove("loading");
-        submitBtn.innerHTML = "Odbierz paczkę";
-        codeMsg.innerHTML = "Niepoprawna wartość";
-        code.classList.add("error");
-    }
-    else {
-        codeMsg.innerHTML = "";
-        code.classList.remove("error");
-    }
-
     if (phoneResult == true && codeResult == true){
         modal.classList.contains("none") && modal.classList.remove("none");
         phone.value="";
@@ -68,10 +109,19 @@ function validate() {
         submitBtn.classList.remove("loading");
         submitBtn.innerHTML = "Odbierz paczkę";
     }
+    else {
+        submitBtn.classList.remove("loading");
+        submitBtn.innerHTML = "Odbierz paczkę";
+        submitBtn.disabled = true;
+    }
 }
 
 submitBtn.addEventListener("click", function(){
     submitBtn.classList.add("loading");
     submitBtn.innerHTML = "";
-    setTimeout(validate, 2000);
+    checkArray = [];
+    setTimeout(validate, 1500);
 });
+
+phone.addEventListener("focusout", validatePhone);
+code.addEventListener("focusout", validateCode);
